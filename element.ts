e6822cac0d9d986e7217bf584b9a6cc9c91f3b7a
alden12 <ablaslett@gmail.com>
2020-01-08 22:08:46 +0000

    Updated children pipe

--- element.ts
+++ element.ts
@@ -122,19 +122,19 @@ export function text(text: Observable<string> | string): Observable<Text> {
   );
 }
 
-export type Children<T extends Node> =
-  // string
-  // | Observable<string>
-  | Observable<Node>
-  | Observable<Node>[]
-  | Observable<Observable<Node>[]>
-  | ((el: T) => Observable<Node>[])
-  | ((el: T) => Observable<Observable<Node>[]>);
+// export type Children<T extends Node> =
+//   // string
+//   // | Observable<string>
+//   | Observable<Node>
+//   | Observable<Node>[]
+//   | Observable<Observable<Node>[]>
+//   | ((el: T) => Observable<Node>[])
+//   | ((el: T) => Observable<Observable<Node>[]>);
 
-export type NodeTypes = Node | string | (Node | string)[];
-export type ChildElement = | NodeTypes | Observable<NodeTypes>;
+// export type NodeTypes = Node | string | (Node | string)[];
+export type ChildElement = Node | string | (Node | string)[] | Observable<Node | Node[]>;
 
-export function node<K extends keyof HTMLElementTagNameMap>(
+export function element<K extends keyof HTMLElementTagNameMap>(
   tagName: K
 ): Observable<HTMLElementTagNameMap[K]> {
   return of(document.createElement(tagName));
@@ -145,12 +145,23 @@ export function coerceChildElement<T extends HTMLElement>(
   el: T,
 ): Observable<Node[]> {
   const childElement = typeof child === 'function' ? child(el) : child;
-  const nodes$ = childElement instanceof Observable ? childElement : of(childElement);
-  return nodes$.pipe(
-    map(nodes => Array.isArray(nodes) ? nodes : [nodes]),
-    // Passing in a string observable should not have to recreate text node each change.
-    map((nodes) => nodes.map(node => typeof node === 'string' ? document.createTextNode(node) : node)),
-  );
+  // let nodeArray$: Observable<Node[]>;
+  if (childElement instanceof Observable) {
+    return childElement.pipe(
+      map(nodeOrNodeArray => Array.isArray(nodeOrNodeArray) ? nodeOrNodeArray : [nodeOrNodeArray]),
+    )
+  } else {
+    const nodeOrStringArray = Array.isArray(childElement) ? childElement : [childElement];
+    const nodeArray = nodeOrStringArray.map(
+      nodeOrString => typeof nodeOrString === 'string' ? document.createTextNode(nodeOrString) : nodeOrString);
+    return of(nodeArray);
+  }
+  // const nodes$ = childElement instanceof Observable ? childElement : of(childElement);
+  // return nodes$.pipe(
+  //   map(nodes => Array.isArray(nodes) ? nodes : [nodes]),
+  //   // Passing in a string observable should not have to recreate text node each change.
+  //   map((nodes) => nodes.map(node => typeof node === 'string' ? document.createTextNode(node) : node)),
+  // );
 }
 
 export function updateElementChildren<T extends HTMLElement>(
@@ -173,7 +184,7 @@ export function updateElementChildren<T extends HTMLElement>(
 }
 
 export function children<T extends HTMLElement>(
-  ...children: ChildElement<T>[]
+  ...children: (ChildElement | ((el: T) => ChildElement))[]
 ): (node: Observable<T>) => Observable<T> {
   return (node: Observable<T>): Observable<T> => node.pipe(
     switchMap(el => {
@@ -182,7 +193,7 @@ export function children<T extends HTMLElement>(
           debounceTime(0),
         );
         const nodes$ = unflattenedNodes$.pipe(
-          map(unflattened => unflattened.reduce<Node[]>((flat, nodeArr) => [...flat, ...nodeArr], [])),
+          map(unflattened => unflattened.reduce<Node[]>((flat, nodeArr) => flat.concat(nodeArr), [])),
         );
         return nodes$.pipe(
           map(nodes => updateElementChildren(el, nodes)),
@@ -225,89 +236,89 @@ export function attributes<T extends HTMLElement>(
 }
 
 // TODO: Attributes.
-export function element<K extends keyof HTMLElementTagNameMap>(
-  tagName: K,
-  children: Children<HTMLElementTagNameMap[K]>
-): Observable<HTMLElementTagNameMap[K]>;
-export function element<K extends keyof HTMLElementTagNameMap>(
-  tagName: K,
-  attributes: Attributes | Observable<Attributes>,
-  children: Children<HTMLElementTagNameMap[K]>
-): Observable<HTMLElementTagNameMap[K]>;
-export function element<K extends keyof HTMLElementTagNameMap>(
-  tagName: K,
-  attributesOrChildren:
-    | Attributes
-    | Observable<Attributes>
-    | Children<HTMLElementTagNameMap[K]>,
-  childrenOrUndefined?: Children<HTMLElementTagNameMap[K]>
-): Observable<HTMLElementTagNameMap[K]> {
-  let attributes: Attributes | Observable<Attributes>;
-  let children: Children<HTMLElementTagNameMap[K]>;
-  if (childrenOrUndefined) {
-    attributes = attributesOrChildren as Attributes | Observable<Attributes>;
-    children = childrenOrUndefined;
-  } else {
-    attributes = {};
-    children = attributesOrChildren as Children<HTMLElementTagNameMap[K]>;
-  }
+// export function element<K extends keyof HTMLElementTagNameMap>(
+//   tagName: K,
+//   children: Children<HTMLElementTagNameMap[K]>
+// ): Observable<HTMLElementTagNameMap[K]>;
+// export function element<K extends keyof HTMLElementTagNameMap>(
+//   tagName: K,
+//   attributes: Attributes | Observable<Attributes>,
+//   children: Children<HTMLElementTagNameMap[K]>
+// ): Observable<HTMLElementTagNameMap[K]>;
+// export function element<K extends keyof HTMLElementTagNameMap>(
+//   tagName: K,
+//   attributesOrChildren:
+//     | Attributes
+//     | Observable<Attributes>
+//     | Children<HTMLElementTagNameMap[K]>,
+//   childrenOrUndefined?: Children<HTMLElementTagNameMap[K]>
+// ): Observable<HTMLElementTagNameMap[K]> {
+//   let attributes: Attributes | Observable<Attributes>;
+//   let children: Children<HTMLElementTagNameMap[K]>;
+//   if (childrenOrUndefined) {
+//     attributes = attributesOrChildren as Attributes | Observable<Attributes>;
+//     children = childrenOrUndefined;
+//   } else {
+//     attributes = {};
+//     children = attributesOrChildren as Children<HTMLElementTagNameMap[K]>;
+//   }
 
-  const el = document.createElement(tagName);
-  const childrenObservable =
-    typeof children === "function" ? children(el) : children;
-  const childrenArrayObservable = Array.isArray(childrenObservable)
-    ? of(childrenObservable)
-    : childrenObservable;
-  const childrenArray = (childrenArrayObservable as Observable<
-    Node | Observable<Node>[]
-  >).pipe(
-    map(nodeOrEls => (Array.isArray(nodeOrEls) ? nodeOrEls : [of(nodeOrEls)])),
-    map(elementes => elementes.filter(el => el)),
-    switchMap(elementes => combineLatest<Node[]>(...elementes)),
-    map(nodes => nodes.filter(node => node)),
-    debounceTime(0)
-  );
+//   const el = document.createElement(tagName);
+//   const childrenObservable =
+//     typeof children === "function" ? children(el) : children;
+//   const childrenArrayObservable = Array.isArray(childrenObservable)
+//     ? of(childrenObservable)
+//     : childrenObservable;
+//   const childrenArray = (childrenArrayObservable as Observable<
+//     Node | Observable<Node>[]
+//   >).pipe(
+//     map(nodeOrEls => (Array.isArray(nodeOrEls) ? nodeOrEls : [of(nodeOrEls)])),
+//     map(elementes => elementes.filter(el => el)),
+//     switchMap(elementes => combineLatest<Node[]>(...elementes)),
+//     map(nodes => nodes.filter(node => node)),
+//     debounceTime(0)
+//   );
 
-  const processChildren = childrenArray.pipe(
-    map(children_ => {
-      const diff = childDiffer(Array.from(el.childNodes.values()), children_);
-      diff.removed.forEach(node => el.removeChild(node));
-      el.append(
-        ...diff.updated
-          .filter(update => !update.insertBefore)
-          .map(update => update.node)
-      );
-      diff.updated
-        .filter(update => update.insertBefore)
-        .forEach(update => el.insertBefore(update.node, update.insertBefore));
+//   const processChildren = childrenArray.pipe(
+//     map(children_ => {
+//       const diff = childDiffer(Array.from(el.childNodes.values()), children_);
+//       diff.removed.forEach(node => el.removeChild(node));
+//       el.append(
+//         ...diff.updated
+//           .filter(update => !update.insertBefore)
+//           .map(update => update.node)
+//       );
+//       diff.updated
+//         .filter(update => update.insertBefore)
+//         .forEach(update => el.insertBefore(update.node, update.insertBefore));
 
-      return el;
-    })
-  );
+//       return el;
+//     })
+//   );
 
-  const attributesObservable =
-    attributes instanceof Observable ? attributes : of(attributes);
+//   const attributesObservable =
+//     attributes instanceof Observable ? attributes : of(attributes);
 
-  let previousAttributes: Attributes = {};
-  const processAttributes = attributesObservable.pipe(
-    map(attributes_ => {
-      const diff = attributeDiffer(previousAttributes, attributes_);
-      previousAttributes = attributes_;
-      Object.keys(diff.updated).forEach(key => {
-        el.setAttribute(key, diff.updated[key]);
-      });
-      diff.removed.forEach(attr => el.removeAttribute(attr));
+//   let previousAttributes: Attributes = {};
+//   const processAttributes = attributesObservable.pipe(
+//     map(attributes_ => {
+//       const diff = attributeDiffer(previousAttributes, attributes_);
+//       previousAttributes = attributes_;
+//       Object.keys(diff.updated).forEach(key => {
+//         el.setAttribute(key, diff.updated[key]);
+//       });
+//       diff.removed.forEach(attr => el.removeAttribute(attr));
 
-      return el;
-    })
-  );
+//       return el;
+//     })
+//   );
 
-  return merge(processChildren, processAttributes).pipe(distinctUntilChanged());
-}
+//   return merge(processChildren, processAttributes).pipe(distinctUntilChanged());
+// }
 
-export function div(children: Children<HTMLDivElement>) {
-  return element("div", children);
-}
+// export function div(children: Children<HTMLDivElement>) {
+//   return element("div", children);
+// }
 
 export const app = () => {
   // const content = (el: Node) =>
@@ -318,7 +329,7 @@ export const app = () => {
   //   );
 
   // return div(element("div", { style: "color: red" }, text("hello")));
-  return node('div').pipe(
+  return element('div').pipe(
     attributes({ style: 'color: red' }),
     children(
       'hello world',
