bf531fc9577aa2f035eaa0e7828f4bbf7966db1c
alden12 <ablaslett@gmail.com>
2020-02-04 22:06:01 +0000

    Added update element children function to children file

--- component/children/children.ts
+++ component/children/children.ts
@@ -1,6 +1,7 @@
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IComponent } from '../component';
+import { childDiffer } from './child-differ';
 
 export type ChildComponent<E = undefined> = string | number | Observable<string | number | IComponent<any, E> | IComponent<any, E>[]>;
 
@@ -22,4 +23,24 @@ export function coerceChildComponent<E = undefined>(childComponent: ChildCompone
     const node = document.createTextNode(content);
     return of([{ node }]);
   }
+}
+
+export function updateElementChildren<T extends HTMLElement>(
+  el: T,
+  previousChildren: Node[],
+  newChildren: Node[]
+): T {
+  const diff = childDiffer(previousChildren, newChildren);
+
+  diff.removed.forEach(node => el.removeChild(node));
+  el.append(
+    ...diff.updated
+      .filter(update => !update.insertBefore)
+      .map(update => update.node)
+  );
+  diff.updated
+    .filter(update => update.insertBefore)
+    .forEach(update => el.insertBefore(update.node, update.insertBefore));
+
+  return el;
 }
