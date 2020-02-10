ca48179f69bc628f52964700dbe8e549f6aa4847
alden12 <ablaslett@gmail.com>
2020-02-10 18:33:51 +0000

    Started adding more types to event pipe.

--- component/events.ts
+++ component/events.ts
@@ -3,28 +3,42 @@ import { merge, Observable, EMPTY, fromEvent } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export function event<T extends Node, E, O>(
-  event: ((node: T) => Observable<O>),
+  eventFunction: ((node: T) => Observable<O>),
+): ComponentOperator<T, E, E | O>
+export function event<T extends Node, E, O>(
+  event: Observable<O>,
 ): ComponentOperator<T, E, E | O>
 export function event<T extends Node, E>(
   eventType: string,
 ): ComponentOperator<T, E, E | Event>
 export function event<T extends Node, E, O>(
   eventType: string,
-  mappingFunction: (event: Event) => O,
+  mappingFunction: (event: Observable<Event>) => Observable<O>,
 ): ComponentOperator<T, E, E | O>
 export function event<T extends Node, E, O>(
-  event: ((node: T) => Observable<O>) | string,
-  mappingFunction?: (event: Event) => O,
+  event: ((node: T) => Observable<O>) | Observable<O> | string,
+  mappingFunction?: (event: Observable<Event>) => Observable<O>,
 ): ComponentOperator<T, E, E | O> {
   return (component: Component<T, E>) => component.pipe(
     map(({ node, events }) => ({
       node,
       events: merge<E>(
         events || EMPTY,
-        typeof event === 'string'
-          ? mappingFunction ? fromEvent(node, event).pipe(map(mappingFunction)) : fromEvent(node, event)
-          : event(node),
+        getEvents(node, event, mappingFunction),
       ),
     })),
   );
+}
+
+function getEvents<T extends Node, O>(
+  node: T,
+  event: string | Observable<O> | ((node: T) => Observable<O>),
+  mappingFunction: (event: Observable<Event>) => Observable<O>,
+): Observable<O | Event> {
+  if (typeof event === 'string') {
+    return mappingFunction ? fromEvent(node, event).pipe(mappingFunction) : fromEvent(node, event);
+  } else if (event instanceof Observable) {
+    return event;
+  }
+  return event(node);
 }
