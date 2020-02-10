823b58a44e268376c751a36f3cec7c64157b7709
alden12 <ablaslett@gmail.com>
2020-02-10 11:19:07 +0000

    Added overloads to event operator.

--- component/events.ts
+++ component/events.ts
@@ -2,15 +2,28 @@ import { ComponentOperator, Component } from './';
 import { merge, Observable, EMPTY, fromEvent } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export function event<T extends Node, E = undefined>(
+export function event<T extends Node, E>(
+  event: ((node: T) => Observable<E>),
+): ComponentOperator<T, E>
+export function event<T extends Node, E>(
+  eventType: string,
+): ComponentOperator<T, E & Event>
+export function event<T extends Node, E>(
+  eventType: string,
+  mappingFunction: (event: Event) => E,
+): ComponentOperator<T, E>
+export function event<T extends Node, E>(
   event: ((node: T) => Observable<E>) | string,
+  mappingFunction?: (event: Event) => E,
 ): ComponentOperator<T, E> {
   return (component: Component<T, E>) => component.pipe(
     map(({ node, events }) => ({
       node,
       events: merge<E>(
         events || EMPTY,
-        typeof event === 'string' ? fromEvent(node, event) : event(node),
+        typeof event === 'string'
+          ? mappingFunction ? fromEvent(node, event).pipe(map(mappingFunction)) : fromEvent(node, event)
+          : event(node),
       ),
     })),
   );
