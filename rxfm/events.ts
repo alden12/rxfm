e98118315852eeb362e45213c991fc6653c85bd7
alden12 <ablaslett@gmail.com>
2020-02-18 17:26:20 +0000

    Started converting to event and types

--- rxfm/events.ts
+++ rxfm/events.ts
@@ -4,28 +4,34 @@ import { map, share, filter } from 'rxjs/operators';
 
 export function event<T extends Node, E, O>(
   eventFunction: ((node: T) => Observable<O>),
-): ComponentOperator<T, E, E | O>
+): ComponentOperator<T, Partial<E>, Partial<E & O>>
+
 export function event<T extends Node, E, O>(
   event: Observable<O>,
-): ComponentOperator<T, E, E | O>
+): ComponentOperator<T, Partial<E>, Partial<E & O>>
+
 export function event<T extends Node, E, K extends keyof HTMLElementEventMap>(
   eventType: K,
-): ComponentOperator<T, E, E | HTMLElementEventMap[K]>
-export function event<T extends Node, E>(
-  eventType: string,
-): ComponentOperator<T, E, E | Event>
+): ComponentOperator<T, Partial<E>, Partial<E & {[eventType]: HTMLElementEventMap[K]}>>
+
+export function event<T extends Node, E, K extends string>(
+  eventType: K,
+): ComponentOperator<T, Partial<E>, Partial<E & {[eventType]: Event}>>
+
 export function event<T extends Node, E, O, K extends keyof HTMLElementEventMap>(
   eventType: K,
   mappingFunction: (event: Observable<HTMLElementEventMap[K]>) => Observable<O>,
-): ComponentOperator<T, E, E | O>
+): ComponentOperator<T, Partial<E>, Partial<E & O>>
+
 export function event<T extends Node, E, O>(
   eventType: string,
   mappingFunction: (event: Observable<Event>) => Observable<O>,
-): ComponentOperator<T, E, E | O>
+): ComponentOperator<T, Partial<E>, Partial<E & O>>
+
 export function event<T extends Node, E, O>(
   event: ((node: T) => Observable<O>) | Observable<O> | string,
   mappingFunction?: (event: Observable<Event>) => Observable<O>,
-): ComponentOperator<T, E, E | O> {
+): ComponentOperator<T, Partial<E>, Partial<E & O>> {
   return (component: Component<T, E>) => component.pipe(
     map(({ node, events }) => ({
       node,
@@ -43,9 +49,13 @@ function getEvents<T extends Node, O>(
   node: T,
   event: string | Observable<O> | ((node: T) => Observable<O>),
   mappingFunction?: (event: Observable<Event>) => Observable<O>,
-): Observable<O | Event> {
+): Observable<O | { [type: string]: Event }> {
   if (typeof event === 'string') {
-    return mappingFunction ? fromEvent(node, event).pipe(mappingFunction) : fromEvent(node, event);
+    return mappingFunction
+      ? fromEvent(node, event).pipe(mappingFunction)
+      : fromEvent(node, event).pipe(
+        map(ev => ({ [event]: ev })),
+      );
   } else if (typeof event === 'function') {
     return event(node);
   }
