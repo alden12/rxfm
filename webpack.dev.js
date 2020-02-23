e3239eeb713267d3f6a1fddf41fdd13fd30a4348
Alden <ablaslett@gmail.com>
2020-02-23 13:18:41 +0000

    Started adding lib build for rxfm

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,36 +1,59 @@
 const path = require('path');
 
-module.exports = {
-  mode: "development",
-	devtool: "inline-source-map",
-	devServer: {
-		contentBase: path.join(__dirname, "dist"),
-		port: 3000,
-		compress: true
+module.exports = [
+  {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      port: 3000,
+      compress: true
+    },
+    entry: './src/app/app.ts',
+    output: {
+      filename: 'app.bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+      extensions: [".ts", ".js", ".json"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: "ts-loader",
+          exclude: '/node_modules/'
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+          exclude: '/node_modules/'
+        },
+      ]
+    }
   },
-  entry: {
-    app: './src/app/app.ts',
-    rxfm: './src/rxfm/index.ts',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: [".ts", ".js", ".json"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-        exclude: '/node_modules/'
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-        exclude: '/node_modules/'
-      },
-    ]
+  {
+    mode: "development",
+    devtool: "inline-source-map",
+    optimization: {
+      runtimeChunk: true
+    },
+    entry: "./src/rxfm/index.ts",
+    output: {
+      filename: 'rxfm.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+      extensions: [".ts", ".js", ".json"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: "ts-loader",
+          exclude: '/node_modules/'
+        },
+      ]
+    }
   }
-};
+];
