46e042bf9dfc745d1aced5740b0492b65dc47876
Alden Laslett <ablaslett@gmail.com>
2021-03-07 22:21:34 +0000

    Added docs for metadata service

--- src/lib/rxfm/metadata-service.ts
+++ src/lib/rxfm/metadata-service.ts
@@ -1,26 +1,51 @@
-import { AttributeObject, StyleDictionary } from "./attributes";
+import { AttributeDictionary, StyleDictionary } from "./attributes";
 import { ChildrenBlockMetadata } from "./children/children-metadata";
 import { ElementType } from "./components";
 
+/**
+ * A map of styles operator symbols to dictionaries of style names to values requested by each styles operator.
+ */
+type StylesMetadataMap = Map<symbol, StyleDictionary>;
+
+/**
+ * A map of attributes operator symbols to dictionaries of attribute names to values requested by each attributes operator.
+ */
+type AttributesMetadataMap = Map<symbol, AttributeDictionary>;
+
+/**
+ * A map of classes operator symbols to sets of class name strings requested by each classes operator.
+ */
+type ClassesMetadataMap = Map<symbol, Set<string>>;
+
+/**
+ * A class to contain the operator metadata for a element.
+ */
 class ElementMetadata {
-  public styles = new Map<symbol, StyleDictionary>();
-  public attributes = new Map<symbol, AttributeObject>();
-  public classes = new Map<symbol, Set<string>>();
+  public styles: StylesMetadataMap = new Map<symbol, StyleDictionary>();
+  public attributes: AttributesMetadataMap = new Map<symbol, AttributeDictionary>();
+  public classes: ClassesMetadataMap = new Map<symbol, Set<string>>();
   public children: ChildrenBlockMetadata[] = [];
 }
 
+/**
+ * A service class to handle the operator metadata for each RxFM DOM element.
+ */
 class ElementMetadataService {
+  /**
+   * A weak map of DOM elements to metadata objects, must be a weak map so that garbage collection is not prevented when elements
+   * are removed from the DOM and go out of scope.
+   */
   protected elementMetadataMap = new WeakMap<ElementType, ElementMetadata>();
 
-  public getStylesMap(element: ElementType): Map<symbol, StyleDictionary> {
+  public getStylesMap(element: ElementType): StylesMetadataMap {
     return this.getMetadata(element).styles;
   }
 
-  public getAttributesMap(element: ElementType): Map<symbol, StyleDictionary> {
+  public getAttributesMap(element: ElementType): AttributesMetadataMap {
     return this.getMetadata(element).attributes;
   }
 
-  public getClassesMap(element: ElementType): Map<symbol, Set<string>> {
+  public getClassesMap(element: ElementType): ClassesMetadataMap {
     return this.getMetadata(element).classes;
   }
 
@@ -32,6 +57,9 @@ class ElementMetadataService {
     this.getMetadata(element).children = metadata;
   }
 
+  /**
+   * Get metadata for an element, element metadata will be created and added first if not present.
+   */
   private getMetadata(element: ElementType): ElementMetadata {
     const metadata = this.elementMetadataMap.get(element);
     if (!metadata) {
@@ -43,8 +71,14 @@ class ElementMetadataService {
   }
 }
 
+/**
+ * The single instance of the element metadata service to be used around the application.
+ */
 export const elementMetadataService = new ElementMetadataService();
 
+/**
+ * Equivalent to the element metadata service class but with extra methods used for testing.
+ */
 export class TestElementMetadataService extends ElementMetadataService {
   public inspectMetadata(element: ElementType) {
     return this.elementMetadataMap.get(element);
