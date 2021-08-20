77db2bcc0b4344d07d3765dbbe57c32006618e4f
Alden Laslett <ablaslett@gmail.com>
2021-08-20 18:09:37 +0100

    Added attributes for intrinsic JSX elements, fixed type issue with FC children, updated jsx example.

--- src/app/basic-examples/jsx-example.tsx
+++ src/app/basic-examples/jsx-example.tsx
@@ -2,12 +2,12 @@
 import RxFM, { FC } from "rxfm";
 import { timer } from "rxjs";
 
-const Greet: FC<{ name: string }> = ({ name }) => <span style={{ color: "blue" }}>Hello {name}!</span>;
+const Greet: FC = ({ children }) => <span style={{ color: "blue" }}>Hello {children}!</span>;
 
-const Timer = () => <span>Time Elapsed: {timer(0, 1000)}s</span>;
+const Timer: FC<{ period: number }> = ({ period }) => <span>Time Elapsed: {timer(0, period)}s</span>;
 
-export const JSXExample = <div>
-  <Greet name="JSX" />
+export const JSXExample = <div id="jsx-example">
+  <Greet>JSX</Greet>
   <br />
-  <Timer />
+  <Timer period={1000} />
 </div>;
--- src/lib/rxfm/rxfm-jsx.ts
+++ src/lib/rxfm/rxfm-jsx.ts
@@ -2,12 +2,12 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import { ComponentChild, Component, Styles, StyleObject, ClassType } from "rxfm";
 import { Observable } from "rxjs";
-import { AttributeObject, attributes, Attributes, classes, styles } from "./attributes";
+import { AttributeObject, attributes, Attributes, AttributeType, classes, HTMLAttributes, styles } from "./attributes";
 import { htmlComponentCreator } from "./components";
-import { coerceToArray } from "./utils";
+import { coerceToArray, flatten, PartialRecord, TypeOrObservable } from "./utils";
 
 export interface DefaultProps {
-  children?: ComponentChild | ComponentChild[];
+  children?: ComponentChild | (ComponentChild | ComponentChild[])[];
   style?: Styles | Observable<StyleObject>;
   class?: ClassType | ClassType[];
   attributes?: Attributes | Observable<AttributeObject>;
@@ -15,33 +15,56 @@ export interface DefaultProps {
 
 declare namespace RxFM {
   namespace JSX {
-    interface IntrinsicElements extends Record<keyof HTMLElementTagNameMap, DefaultProps> {}
+    interface Element extends Component {}
     interface ElementAttributesProperty { props: {}; }
     interface ElementChildrenAttribute { children: {}; }
-    interface Element extends Component {}
+    interface IntrinsicElements extends Record<
+      keyof HTMLElementTagNameMap,
+      DefaultProps & PartialRecord<keyof HTMLAttributes, TypeOrObservable<AttributeType>>
+    > {}
+    interface IntrinsicAttributes extends DefaultProps {}
   }
 }
 
+export interface FCProps extends Omit<DefaultProps, 'children'> {
+  children?: ComponentChild | ComponentChild[];
+}
+
+// TODO: Find a way to allow children type to be redefined?
+export type FC<T extends Record<string, any> = {}> = (props: FCProps & T) => RxFM.JSX.Element;
+
 function createElement(tagName: keyof HTMLElementTagNameMap, props: DefaultProps, ...children: ComponentChild[]): RxFM.JSX.Element;
 function createElement<T>(fc: FC<T>, props: DefaultProps & T, ...children: ComponentChild[]): RxFM.JSX.Element;
-// TODO: Overload for class component?
 function createElement<T = Record<string, never>>(
   tagOrFc: keyof HTMLElementTagNameMap | FC<T>,
   props: DefaultProps & T,
-  ...children: ComponentChild[]
+  ...children: (ComponentChild | ComponentChild[])[]
 ): RxFM.JSX.Element {
-  // TODO: How to detect SVG elements? Have a list of available SVG elements?
-  // TODO: How to detect which props should be attributes? Have a list of all attributes?
-  const component = typeof tagOrFc === 'function' ? tagOrFc(props) : htmlComponentCreator(tagOrFc)(...coerceToArray(children));
+  let component: RxFM.JSX.Element;
+  
+  const filteredProps = { ...props };
+  delete filteredProps.style;
+  delete filteredProps.class;
+  delete filteredProps.attributes;
+
+  if (typeof tagOrFc === 'function') {
+    component = tagOrFc({ ...filteredProps, children: flatten<ComponentChild>(children) });
+  } else if (typeof tagOrFc === 'string') {
+    // TODO: How to detect SVG elements? Have a list of available SVG elements?
+    component = htmlComponentCreator(tagOrFc)(...flatten(children)).pipe(
+      (Object.keys(filteredProps).length ? attributes(filteredProps) : src => src)
+    );
+  } else {
+    throw new TypeError(`Invalid type passed as JSX tag. Expected string or FC, received: ${typeof tagOrFc}.`);
+  }
+
   return component.pipe(
-    classes(...coerceToArray(props?.class)),
-    styles(props?.style || {}),
-    attributes(props?.attributes || {}),
+    (props?.class ? classes(...coerceToArray(props.class)) : src => src),
+    (props?.style ? styles(props.style || {}) : src => src),
+    (props?.attributes ? attributes(props.attributes) : src => src),
   );
 }
 
 const RxFM = { createElement };
-
-export type FC<T extends Record<string, any>> = (props: T & DefaultProps) => RxFM.JSX.Element;
 
 export default RxFM;
